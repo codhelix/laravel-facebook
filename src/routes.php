2e6b9db72250644c887949195b4363ddf0f1f909
Paco G. Navarro <paco.gn@gmail.com>
2014-01-02 20:48:52 +0100

    add constants for more easy calls

--- src/routes.php
+++ src/routes.php
@@ -1,4 +1,6 @@
 <?php
+define ('WITH_REDIRECT_SCRIPT', true);
+define ('WITH_REQUEST_PARAMS', true);
 
 Route::get('/channel.html', function(){
 	return '<script src="//connect.facebook.net/'. Config::get('laravel-facebook::locale') .'/all.js"></script>';
